681371c4770b8dc554f6b3cdbe9395de7c306ee9
Amine Zerouali <amine.zerouali@polymtl.ca>
2023-01-17 15:19:18 -0500

    commentaire ajouté

--- comparison.h
+++ comparison.h
@@ -1,7 +1,6 @@
-
+// This is the function to call when comparing the newton approximation to the real root.
 
 
 float comparison(float initial_value, float root);
 
 float comparison_absolutie(float initial_value, float root);
-
